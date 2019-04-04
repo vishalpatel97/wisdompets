e493e83e18ae05153d3e11e877006ddd118104c4
vishalpatel97 <vishalpatel.2k14@gmail.com>
2019-04-04 15:41:34 -0500

    Done: url handlers, views and templates

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pet
 
 # Create your views here.
+def home(req):
+    pets = Pet.objects.all()
+    # return HttpResponse('<p>Home view</p>')
+    return render(req, 'home.html', {'pets': pets})
+
+def pet_detail(req, id):
+    try:
+        pet = Pet.objects.get(id = id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+    return render(req, 'pet_detail.html', {'pet': pet})
--- wisdompets/urls.py
+++ wisdompets/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import re_path
+
+from adoptions import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path('admin/', admin.site.urls),
+    re_path(r'^$', views.home, name='home'),
+    re_path(r'^adoptions/(\d+)/', views.pet_detail, name='pet_detail'),
 ]
